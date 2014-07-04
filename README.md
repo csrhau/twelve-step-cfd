dfe8d625823ef12a0b98d94efef09e1f40b789dd
Stephen Roberts <stephenianroberts@gmail.com>
2014-07-04 16:11:08 +0100

    Step 0 Completed

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 twelve-step-cfd
 ===============
-
 Following http://lorenabarba.com/blog/cfd-python-12-steps-to-navier-stokes/
+
+Step 0: Initial Commit
+
+
+Technologies To Be Used:
+==============
++ h5py - http://docs.h5py.org/en/latest/quick.html
+  - High density data storage. Use this to store out simulation vectors
---
+++ steps/step0/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+""" 12 steps to cfd from https://github.com/csrhau/twelve-step-cfd.git """
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import time
+
+def advection_sim():
+    """ 1 dimensional forwards discretized scheme """
+    nx = 41      # Number of x steps
+    dx = 2.0/(nx-1) # Length of each step (plane is 2 units long)
+    nt = 25    #nt is the number of timesteps we want to calculate
+    dt = 0.025  #dt is the amount of time each timestep covers (delta t)
+    c = 1.0      #assume wavespeed of c = 1
+    u = np.ones(nx)
+    u[.5/dx : 1/dx+1]=2  #setting u = 2 between 0.5 and 1 as per our I.C.s
+    plt.plot(np.linspace(0, 2, nx), u)
+    for t in range(nt):
+      print(t)
+      un = u.copy()
+      for i in range(1, nx):
+        u[i] = un[i] - c * (dt/dx) * (un[i] - un[i-1])
+    plt.plot(np.linspace(0, 2, nx), u)
+    plt.show()
+
+def main():
+    """ Application Entry Point"""
+    advection_sim() 
+    print('done')
+
+if __name__ == '__main__':
+    main()
